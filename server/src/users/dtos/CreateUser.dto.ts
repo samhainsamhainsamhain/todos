5c3bc32b02383a6f990b06f1d1efaa2b2b1ff389
Samhain <rikalashnikov@gmail.com>
2022-09-12 12:04:27 +0300

    dto and type to createUser

---
+++ server/src/users/dtos/CreateUser.dto.ts
@@ -0,0 +1,12 @@
+import { IsNotEmpty, Matches } from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  username: string;
+
+  @IsNotEmpty()
+  password: string;
+
+  @IsNotEmpty()
+  confirmPassword: string;
+}
