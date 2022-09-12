1c5bd71a62d4d7261cfd88a74da7f2f02a116b18
Samhain <rikalashnikov@gmail.com>
2022-09-12 10:29:09 +0300

    add mysql settings, orm for User, update gitignore

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'admin',
+      password: 'admin',
+      database: 'todonet_mysql',
+      entities: [],
+      synchronize: true,
+    }),
+    UsersModule,
+  ],
   controllers: [],
   providers: [],
 })
