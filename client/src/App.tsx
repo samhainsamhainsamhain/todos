27858892cfbd7e57e29678f0a2160adaf943092f
Samhain <rikalashnikov@gmail.com>
2022-10-12 19:49:57 +0300

    implement auth component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,12 @@
 import './App.css';
+import Auth from './components/auth/Auth';
 
 function App() {
-  return <div className="App">Hello world!</div>;
+  return (
+    <div className="App">
+      <Auth />
+    </div>
+  );
 }
 
 export default App;
