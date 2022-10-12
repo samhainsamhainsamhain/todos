5575683f7bd9c7ba2892ab84ce2259f3095e8cb4
Samhain <rikalashnikov@gmail.com>
2022-10-12 14:47:16 +0300

    add router

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,16 @@
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
 import App from './App';
 import './index.css';
 
+import { store } from './app/store';
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <App />
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>
 );
