e5bb8ae51e1e6589c093b96ce90a871571f9068a
Samhain <rikalashnikov@gmail.com>
2022-10-13 16:36:30 +0300

    axios methods for Auth

---
+++ client/src/api/api.ts
@@ -0,0 +1,16 @@
+import axios, { AxiosRequestConfig } from 'axios';
+import { CreateUserParams, User, UserCredentialsParams } from '../types/User';
+
+const API_URL = 'http://localhost:3001';
+// process.env.REACT_APP_API_URL; // TODO get variables from env, currently undefined
+
+const config: AxiosRequestConfig = { withCredentials: true };
+
+export const postRegisterUser = (data: CreateUserParams) =>
+  axios.post(`${API_URL}/users`, data, config);
+
+export const postLoginUser = (data: UserCredentialsParams) =>
+  axios.post(`${API_URL}/auth/login`, data, config);
+
+export const getAuthUser = () =>
+  axios.get<User>(`${API_URL}/auth/status`, config);
