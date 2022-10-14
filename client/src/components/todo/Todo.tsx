ef24fbf63583d74438e9a625192005546d647a05
Samhain <rikalashnikov@gmail.com>
2022-10-14 11:51:44 +0300

    implement TodoItem component

---
+++ client/src/components/todo/Todo.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { TodoItem } from '../../types/TodoItem';
+
+interface ITodo {
+  todo: TodoItem;
+}
+
+const Todo = ({}: ITodo) => {
+  return <div>Todo</div>;
+};
+
+export default Todo;
---
+++ client/src/types/TodoItem.ts
@@ -0,0 +1,4 @@
+export type TodoItem = {
+  title: string;
+  description?: string;
+};
