8fdd5bc67af02d0068e973d094a118c27c3d9b62
Samhain <rikalashnikov@gmail.com>
2022-10-17 19:17:55 +0300

    and todos slice to index store

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,9 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import todoListsSlice from './todoLists/todoListsSlice';
 import userSlice from './user/userSlice';
+import todosSlice from './todos/todosSlice';
 
 export const store = configureStore({
-  reducer: { userSlice, todoListsSlice },
+  reducer: { userSlice, todoListsSlice, todosSlice },
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
