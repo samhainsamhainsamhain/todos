6a7c96fd023bcd9849c3060c32574d91a5980f40
Samhain <rikalashnikov@gmail.com>
2022-10-17 15:58:58 +0300

    add logout function

--- client/src/components/authenticatedRoute/AuthenticatedRoute.tsx
+++ client/src/components/authenticatedRoute/AuthenticatedRoute.tsx
@@ -1,5 +1,7 @@
-import { FC } from 'react';
-import { Navigate } from 'react-router-dom';
+import { FC, useContext } from 'react';
+import { Navigate, useNavigate } from 'react-router-dom';
+import { logoutUser } from '../../api/api';
+import { AuthContext } from '../../utils/AuthContext';
 import { useAuth } from '../../utils/hooks/useAuth';
 
 export const AuthenticatedRoute: FC<React.PropsWithChildren> = ({
@@ -7,10 +9,26 @@ export const AuthenticatedRoute: FC<React.PropsWithChildren> = ({
 }) => {
   const { loading, user } = useAuth();
 
+  const { updateAuthUser } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  function logOut() {
+    logoutUser();
+    updateAuthUser();
+    navigate('/login');
+  }
+
   if (loading) {
     return <div>loading...</div>;
   }
 
-  if (user) return <>{children}</>;
-  return <Navigate to="/login" state={{ from: location }} replace />;
+  if (!user) return <Navigate to="/login" replace />;
+
+  return (
+    <>
+      <h2>Auth route</h2>
+      <button onClick={logOut}>Logout</button>
+      {children}
+    </>
+  );
 };
