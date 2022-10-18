876c20d7a718cab015aba26f935607270400365a
Samhain <rikalashnikov@gmail.com>
2022-10-18 21:35:01 +0300

    types for todoLists actions

--- client/src/types/TodoList.ts
+++ client/src/types/TodoList.ts
@@ -7,3 +7,18 @@ export type TodoList = {
 export type CreateTodoListParams = {
   title: string;
 };
+
+export type TodoListEventPayload = {
+  todoList: TodoList;
+};
+
+export type UpdateTodoListParams = {
+  id: number;
+  title: string;
+  userid: number;
+};
+
+export type DeleteTodoListParams = {
+  id: number;
+  userid: number;
+};
