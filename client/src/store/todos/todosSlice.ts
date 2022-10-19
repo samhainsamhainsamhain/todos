1faa6153da109979551ce279c6b03e9ceedf505f
Samhain <rikalashnikov@gmail.com>
2022-10-19 18:45:26 +0300

    todo slice reducer and extra-reducer methods

--- client/src/store/todos/todosSlice.ts
+++ client/src/store/todos/todosSlice.ts
@@ -1,8 +1,8 @@
 import { createSlice } from '@reduxjs/toolkit';
 import type { PayloadAction } from '@reduxjs/toolkit';
 import { TodoList } from '../../types/TodoList';
-import { fetchTodos } from './ActionCreators';
-import { TodoItem } from '../../types/TodoItem';
+import { fetchTodosThunk } from './todosThunk';
+import { TodoItem, TodoItemEventPayload } from '../../types/TodoItem';
 
 export interface TodosState {
   todos: TodoItem[];
@@ -19,17 +19,42 @@ const initialState: TodosState = {
 export const todosSlice = createSlice({
   name: 'todos',
   initialState,
-  reducers: {},
+  reducers: {
+    editTodo: (state, action: PayloadAction<TodoItemEventPayload>) => {
+      const newTodo = action.payload.todo;
+      const oldTodo = state.todos.find((todo) => todo.id === newTodo.id);
+      if (!oldTodo) return;
+      const oldTodoListIndex = state.todos.findIndex(
+        (todo) => todo.id === newTodo.id
+      );
+      state.todos[oldTodoListIndex] = newTodo;
+    },
+    removeTodo: (state, action: PayloadAction<TodoItem>) => {
+      const newTodo = action.payload;
+      const oldTodo = state.todos.find((todo) => todo.id === newTodo.id);
+      if (!oldTodo) return;
+      const oldTodoIndex = state.todos.findIndex(
+        (todo) => todo.id === newTodo.id
+      );
+      state.todos[oldTodoIndex] = newTodo;
+    },
+  },
   extraReducers: {
-    [fetchTodos.fulfilled.type]: (state, action: PayloadAction<TodoList[]>) => {
+    [fetchTodosThunk.fulfilled.type]: (
+      state,
+      action: PayloadAction<TodoList[]>
+    ) => {
       state.isLoading = false;
       state.error = '';
       state.todos = action.payload;
     },
-    [fetchTodos.pending.type]: (state, action: PayloadAction<TodoList[]>) => {
+    [fetchTodosThunk.pending.type]: (
+      state,
+      action: PayloadAction<TodoList[]>
+    ) => {
       state.isLoading = true;
     },
-    [fetchTodos.rejected.type]: (state, action: PayloadAction<string>) => {
+    [fetchTodosThunk.rejected.type]: (state, action: PayloadAction<string>) => {
       state.isLoading = false;
       state.error = action.payload;
     },
