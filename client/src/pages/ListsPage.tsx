0c5d96eaeb6a5b12bd83b4d0838c52f8fcdbaccd
Roman K <83346225+samhainsamhainsamhain@users.noreply.github.com>
2022-12-28 00:30:39 +0300

    remove div

--- client/src/pages/ListsPage.tsx
+++ client/src/pages/ListsPage.tsx
@@ -2,8 +2,8 @@ import ListPanel from '../components/lists/ListsPanel';
 
 export const ListsPage = () => {
   return (
-    <div className="lists-page">
+    <>
       <ListPanel />
-    </div>
+    </>
   );
 };
